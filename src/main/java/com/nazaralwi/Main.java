7621efeb300b1482da5151f5acdfbce4832bcc2b
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-07-16 09:00:47 +0700

    Add `binarySearch()` method that implements binary search algorithm and add sorting method as a helper

--- src/main/java/com/nazaralwi/Main.java
+++ src/main/java/com/nazaralwi/Main.java
@@ -1,7 +1,7 @@
 package com.nazaralwi;
 
 public class Main {
-    int temp, pos, i, j, start, end, mid;
+    static int temp, pos, i, j, start, end, mid;
 
     public static int sequential(int[] data, int cari) {
         for (int i = 0; i < data.length; i++) {
@@ -10,5 +10,36 @@ public class Main {
             }
         }
         return -1;
+    }
+
+    public static int binarySearch(int[] data, int cari) {
+        insertionSort(data);
+        start = 0;
+        end = data.length-1;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (data[mid] == cari) {
+                return mid;
+            } else if (data[mid] < cari) {
+                start = mid + 1;
+            } else {
+                end = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int[] insertionSort(int[] data) {
+        int temp, j;
+        for (int i = 0; i < data.length; i++) {
+            temp = data[i];
+            j = i;
+            while ((j > 0) && (data[j-1] > temp)) {
+                data[j] = data[j-1];
+                j--;
+            }
+            data[j] = temp;
+        }
+        return data;
     }
 }
